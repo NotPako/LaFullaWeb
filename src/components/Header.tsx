8242cb92ac6d0dcd0f9dd62f916a3f380e04bafa
PABLO JUAN GRAU <pabjuagr@upv.edu.es>
2025-02-03 19:12:38 +0100

    project created

---
+++ src/components/Header.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import '@/styles/styles.css'
+
+export default function Header() {
+  return (
+    <div className="header">
+   
+        <Image alt='La Fulla' src='/media/LogoGROCFULLA.png' width='100' height = '100'>
+            
+        </Image>
+
+     
+      
+    </div>
+  );
+}
