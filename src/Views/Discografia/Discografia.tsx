ecab858ef819ef1a7800ae9e48197324027d164d
PABLO JUAN GRAU <pabjuagr@upv.edu.es>
2026-01-31 10:35:20 +0100

    discografia en carousel

--- src/Views/Discografia/Discografia.tsx
+++ src/Views/Discografia/Discografia.tsx
@@ -1,21 +1,30 @@
+"use client";
 import React from "react";
 import "./Discografia.css";
 import SpotifyEmbed from "@/components/SpotifyEmbed/SpotifyEmbed";
+import { Carousel } from "antd";
+
+const spotifyItems = [
+  { type: "album", id: "2Z67K7bSlNRCRMNi0Uhwpf" },
+  { type: "track", id: "78yabm3lvNIVAGXzrESIiZ" },
+  { type: "track", id: "4JTdkqI8dn80qYXh8luiGU" },
+  { type: "playlist", id: "37i9dQZF1E4x5GhDbWfThD" },
+] as const;
+
 
 export default function Discografia() {
   return (
     <section className="discografia-section">
-      <h2 className="discografia-title">Discografía</h2>
+      <h2 className="discografia-title">Discografia</h2>
 
-      <div className="spotify-grid">
-        {/* Álbum */}
-        <SpotifyEmbed type="album" id="2Z67K7bSlNRCRMNi0Uhwpf" />
-
-        {/* Single */}
-        <SpotifyEmbed type="track" id="78yabm3lvNIVAGXzrESIiZ" />
-
-        {/* Playlist oficial */}
-        <SpotifyEmbed type="playlist" id="37i9dQZF1E4x5GhDbWfThD" />
+      <div className="carouselDisc-container">
+        <Carousel autoplay dots arrows slidesToShow={3}>
+          {spotifyItems.map((item, index) => (
+            <div key={index} className="carousel-slide spotify-slide">
+              <SpotifyEmbed type={item.type} id={item.id} />
+            </div>
+          ))}
+        </Carousel>
       </div>
     </section>
   );
