f27794efcb67e428cd5cf0603a1a65a0574f4467
PABLO JUAN GRAU <pabjuagr@upv.edu.es>
2026-01-31 10:49:43 +0100

    discografia carousel responsive en movil

--- src/Views/Discografia/Discografia.tsx
+++ src/Views/Discografia/Discografia.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import "./Discografia.css";
 import SpotifyEmbed from "@/components/SpotifyEmbed/SpotifyEmbed";
 import { Carousel } from "antd";
+import { useEffect, useState } from "react";
 
 const spotifyItems = [
   { type: "album", id: "2Z67K7bSlNRCRMNi0Uhwpf" },
@@ -13,12 +14,29 @@ const spotifyItems = [
 
 
 export default function Discografia() {
+
+  function useIsMobile(breakpoint = 768) {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const check = () => setIsMobile(window.innerWidth < breakpoint);
+    check();
+    window.addEventListener("resize", check);
+    return () => window.removeEventListener("resize", check);
+  }, [breakpoint]);
+
+  return isMobile;
+}
+
+  const isMobile = useIsMobile();
+  const slidesToShow = isMobile ? 2 : 3;
+
   return (
     <section className="discografia-section">
       <h2 className="discografia-title">Discografia</h2>
 
       <div className="carouselDisc-container">
-        <Carousel autoplay dots arrows slidesToShow={3}>
+        <Carousel autoplay dots arrows slidesToShow={slidesToShow}>
           {spotifyItems.map((item, index) => (
             <div key={index} className="carousel-slide spotify-slide">
               <SpotifyEmbed type={item.type} id={item.id} />
